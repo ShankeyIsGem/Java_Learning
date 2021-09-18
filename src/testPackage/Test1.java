9ae4eeea95c8ca9f916f258560464d888b5eb130
Shankey <shankeytyagi97@gmail.com>
2021-09-18 07:02:38 -0700

    This is first commit via eclipse

---
+++ src/testPackage/Test1.java
@@ -0,0 +1,26 @@
+package testPackage;
+
+import java.util.Scanner;
+
+public class Test1
+{
+
+	public static void main(String[] args)
+	{
+	   Scanner sr =new Scanner(System.in);
+	   System.out.println("Enter your First Number");
+	   int num1 = sr.nextInt();
+	   System.out.println("Enter your Second Number");
+	   int num2 = sr.nextInt();
+	   if (num1 == num2)
+	   {
+		   System.out.println("Numbers are matching");
+		   
+	   }else {
+		   System.out.println("Numbers are not matching");
+	   }
+	 
+
+	}
+
+}
